26be6c2b32c1b95d5afbb9cd71a80f18294fe4d4
Ararat01 <ararat12bars@gmail.com>
2025-04-13 13:17:06 +0400

    changed to local

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_URL = "https://w2w-backend-ararat01.onrender.com";
-// const API_URL = "http://localhost:4444";
+// const API_URL = "https://w2w-backend-ararat01.onrender.com";
+const API_URL = "http://localhost:4444";
 export default API_URL;
